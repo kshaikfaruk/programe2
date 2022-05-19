ee808945a417cff76d81f649932765876a6329bd
shaikfaruk <shaikfaruk04@gmail.com>
2022-05-19 20:19:31 +0530

    testcase

---
+++ src/Java/Reversenumber.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class Reversenumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=123456;
+int reverse=0;
+while(n!=0){
+	int remainder=n%10;
+	reverse= reverse*10+remainder;
+	n=n/10;
+}
+System.out.println(reverse);
+	}
+
+}
---
+++ src/Java/Stringreverse.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class Stringreverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   String s="Althaf";
+   char c;
+   String rev=" ";
+   for(int i=0;i<s.length();i++){
+	   c=s.charAt(i);
+	   rev=c+rev;
+	   
+   }
+   System.out.println(rev);
+	}
+
+}
